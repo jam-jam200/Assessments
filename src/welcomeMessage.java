e59051cfef85cecf7c6e949056e1aca2b8b754c6
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-06 01:02:45 +0100

    date calculation

--- src/welcomeMessage.java
+++ src/welcomeMessage.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 public class welcomeMessage {
     public static void main(String[] args) {
         //create a scanner class object that can read the user's input from the console
         Scanner scanner = new Scanner(System.in);
-        //prompting user to input details
+        //prompting user to input details(name, gender, date of birth)
         System.out.println("Welcome! Please input the following details accordingly: ");
         System.out.println("Name: ");
         String name = scanner.nextLine();
@@ -13,6 +16,8 @@ public class welcomeMessage {
 
         System.out.println("Date of Birth in this format (dd-mm-yyyy): ");
         String dob = scanner.nextLine();
+
+
 
 
 
