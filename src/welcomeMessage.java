d9a973d59fb969b894c3001ec66912a4e37e5e0d
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-06 17:14:44 +0100

    using calendar class to calculate the user's age

--- src/welcomeMessage.java
+++ src/welcomeMessage.java
@@ -14,15 +14,15 @@ public class welcomeMessage {
         System.out.println("Gender: ");
         String gender = scanner.nextLine();
 
-        System.out.println("Date of Birth in this format (dd-mm-yyyy): ");
+        System.out.println("Date of Birth in this format (YYYY-MM-DD): ");
         String dob = scanner.nextLine();
+
+        //converting user's dob to a LocalDate object
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDateDob = LocalDate.parse(dob, dateTimeFormatter);
 
         //closing scanner to release resources since it's not in use
         scanner.close();
-
-        //converting user's dob to a LocalDate object
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-mm-yyyy");
-        LocalDate localDateDob = LocalDate.parse(dobString, formatter);
 
         //getting the current date using LocalDate
         LocalDate currentDate = LocalDate.now();
@@ -31,7 +31,7 @@ public class welcomeMessage {
         int age = calculateAge(localDateDob, currentDate);
 
         //formatting the current date using the DateTimeFormatter
-        String formattedDate = currentDate.format(DateTimeFormatter.ofPattern("mm-dd-yyyy"));
+        String formattedDate = currentDate.format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
 
         System.out.println("Welcome " + name + ", you are " + age + " years old and today’s date is " + formattedDate);
 
@@ -41,12 +41,18 @@ public class welcomeMessage {
 
     public static int calculateAge(LocalDate localDateDob, LocalDate currentDate) {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(localDateDob.getYear(), localDateDob.getMonthValue() -1, localDateDob.getDayOfMonth());
+        calendar.set(localDateDob.getYear(), localDateDob.getMonthValue() - 1, localDateDob.getDayOfMonth());
 
         Calendar currentCalendar = Calendar.getInstance();
-        currentCalendar.set(currentDate.getYear(), currentDate.getMonthValue(), currentDate.getDayOfMonth());
+        currentCalendar.set(currentDate.getYear(), currentDate.getMonthValue() - 1, currentDate.getDayOfMonth());
 
-
+        int age = currentCalendar.get(Calendar.YEAR) - calendar.get(Calendar.YEAR);
+        if (currentCalendar.get(Calendar.DAY_OF_YEAR) < calendar.get(Calendar.DAY_OF_YEAR)){
+            age--;
+        }
+        return age;
     }
 
+
 }
+
