1bd317049173bc117077abaf725018e9c06946f8
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-06 20:25:44 +0100

    compound interest and comments

--- src/CompoundInterestCalculator.java
+++ src/CompoundInterestCalculator.java
@@ -4,57 +4,63 @@ public class CompoundInterestCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter principal amount
+        //prompting user to enter principal amount
         double principal = getUserInputDouble(scanner, "Enter the principal amount: ");
 
-        // Prompt the user to enter the interest rate
-        double rate = getUserInputDouble(scanner, "Enter the annual interest rate (in percentage): ");
+        //prompting user to enter interest rate
+        double rate = getUserInputDouble(scanner, "Enter the interest rate in percentage: ");
 
-        // Prompt the user to enter the time
-        double time = getUserInputDouble(scanner, "Enter the time: ");
+        //prompting user to enter the time
+        double time = getUserInputDouble(scanner, "Enter the time time: ");
 
-        String timeUnit;
-        // Validate the time unit input using a do-while loop
+        String unit;
+        //validating the time unit inputted by the user using a do-while loop
         do {
-            timeUnit = getUserInputString(scanner, "Enter the time unit (years, days, or months): ");
-        } while (!isValidTimeUnit(timeUnit));
+            unit = getUserInputString(scanner, "Enter the time unit in either years, days, or months): ");
+        } while (!isValidTimeUnit(unit));
 
-        // Calculate the compound interest based on the user's input
-        double compoundInterest = calculateCompoundInterest(principal, rate, time, timeUnit);
+        //calculating the compound interest based on the user's input
+        double compoundInterest = calculateCompoundInterest(principal, rate, time, unit);
 
-        // Display the compound interest to the user
         System.out.println("The compound interest is: " + compoundInterest);
 
+        //closing the scanner to release resources associated with standard input (System.in)
         scanner.close();
     }
 
-    // Method to get user input for a double value
+    //prompts user to enter a double value and validates the input
     public static double getUserInputDouble(Scanner scanner, String prompt) {
         System.out.print(prompt);
+        //keep prompting the user until a valid double value is provided
         while (!scanner.hasNextDouble()) {
+            //if the user enters an invalid value an error message will be displayed
             System.out.print("Invalid input. Please enter a numeric value: ");
             scanner.next();
         }
+        //proceeding with the validated number(valid number)
         return scanner.nextDouble();
     }
 
-    // Method to get user input for a string value (time unit)
+    //method to get user input for the time unit
     public static String getUserInputString(Scanner scanner, String prompt) {
         System.out.print(prompt);
+        //returns the user's input as a lowercase string.
         return scanner.next().toLowerCase();
     }
 
-    // Method to validate the time unit input
+    //method to validate the time unit input
     public static boolean isValidTimeUnit(String timeUnit) {
+        // return true if the time unit is valid either "years", "days", or "months" otherwise false
         return timeUnit.equals("years") || timeUnit.equals("days") || timeUnit.equals("months");
     }
 
-    // Method to calculate compound interest
-    public static double calculateCompoundInterest(double principal, double rate, double time, String timeUnit) {
-        double n; // number of times the interest is compounded per time unit
+    //method to calculate the compound interest
+    public static double calculateCompoundInterest(double principal, double rate, double time, String unit) {
+         //number of times the interest is compounded per time unit
+         double n;
 
         // Convert time to years for calculation
-        switch (timeUnit) {
+        switch (unit) {
             case "days" -> {
                 n = 365.0;
                 time /= 365.0;
