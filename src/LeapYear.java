b340a91d6762bff743f417aae4c8843ecd35a369
jam-jam200 <jessieoyaks4j@gmail.com>
2023-08-07 23:03:38 +0100

    currency converter

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 public class LeapYear {
   public static void main(String[] args) {
     System.out.println("Enter year: ");
+
+    Scanner scanner = new Scanner(System.in);
+    int year = scanner.nextInt();
+
+    public
+
+    boolean isLeapYear = isLeapYear(year);
   }
 }
